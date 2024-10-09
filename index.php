971a61d47ca7c29cc66ea7ddeb7dd8856e6ffed8
Abd71365 <abode.badour77@gmail.com>
2024-10-09 20:26:55 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>project_1</title>
+</head>
+
+<body>
+    <p>welcome to </p> <br>
+    <?php
+    echo "Aleppo";
+
+    $var = "ammar";
+    echo $var;
+    $qqq = "ahmad";
+    echo $qqq;
+
+    $eee="alooo";
+    echo $eee;
+
+
+    ?>
+</body>
+
+</html>
